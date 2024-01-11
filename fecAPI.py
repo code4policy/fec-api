2664fa5b1b89d22c3c37733750c6df9ce92472e9
Aarushi Sahejpal <60757108+aarushisahejpal@users.noreply.github.com>
2024-01-11 10:05:16 -0500

    fec_api

---
+++ fecAPI.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import os
+
+
+# 
+base_url = 'https://api.open.fec.gov/v1/'
+
+# 
+operation = 'receipts'
+
+# 
+key = os.environ['FECKEY']
+
+#
+api_parameters = {'api_key': key, 'office':'H', 'sort':'name', 'state':'MA', 'election_year':[2022]}
+
+# 
+response = requests.get(base_url + operation, params = api_parameters)
+
+# 
+print('Response Code: {0}\n'.format(response.status_code))
+data = json.loads(response.text)
+
+# 
+with open('fec_api_results.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+# 
+for candidate in data['results']:
+    print(candidate['name'])
